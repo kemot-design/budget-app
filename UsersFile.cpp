cee175fe6334aef7371fde9e4a6f31c49e1ef711
Tomasz Budzyński <tomasz.budzynski.programista@gmail.com>
2020-06-17 20:55:36 +0200

    Fixes to saving users to file

--- UsersFile.cpp
+++ UsersFile.cpp
@@ -6,17 +6,32 @@ using namespace std;
 
 void UsersFile::saveUserToFile(User user){
     CMarkup xml;
+    //xml.ResetPos();
 
-    xml.SetDoc("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n");
+    if(xml.Load("Users.xml")){
+        xml.FindElem("Users");
+        xml.IntoElem();
+        while(xml.FindElem("User")){
 
-    xml.AddElem("Users");
-    xml.IntoElem();
-    xml.AddElem("User");
-    xml.IntoElem();
-    xml.AddElem("Name", user.getName());
-    xml.AddElem("Surname", user.getSurname());
-    xml.AddElem("Login", user.getLogin());
-    xml.AddElem("Password", user.getPassword());
-    xml.Save("Users.xml");
-
+        }
+        xml.AddElem("User");
+        xml.IntoElem();
+        xml.AddElem("Name", user.getName());
+        xml.AddElem("Surname", user.getSurname());
+        xml.AddElem("Login", user.getLogin());
+        xml.AddElem("Password", user.getPassword());
+        xml.Save("Users.xml");
+    }
+    else{
+        xml.SetDoc("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n");
+        xml.AddElem("Users");
+        xml.IntoElem();
+        xml.AddElem("User");
+        xml.IntoElem();
+        xml.AddElem("Name", user.getName());
+        xml.AddElem("Surname", user.getSurname());
+        xml.AddElem("Login", user.getLogin());
+        xml.AddElem("Password", user.getPassword());
+        xml.Save("Users.xml");
+    }
 }
